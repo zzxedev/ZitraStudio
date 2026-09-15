750284a87dc651e707b8fe325bf8c86c91f54159
zzxedev <zzxedev@gmail.com>
2026-09-15 19:07:48 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,6 +3,6 @@
 // La clé "anon/public" peut être utilisée dans le navigateur SI les règles RLS du fichier SQL sont installées.
 // NE METS JAMAIS une "service_role key" ici.
 window.ZITRA_CONFIG = {
-  SUPABASE_URL: "https://YOUR_PROJECT.supabase.co",
-  SUPABASE_ANON_KEY: "YOUR_SUPABASE_ANON_KEY"
+  SUPABASE_URL: "https://xsptbqffielijdalzpdx.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhzcHRicWZmaWVsaWpkYWx6cGR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk0ODU5NDksImV4cCI6MjEwNTA2MTk0OX0.mgAHmfiRbtssHWeDIR5Kx79Uw7o3aZFjlwWt0Q44R8Y"
 };
